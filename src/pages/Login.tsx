d930bbe9708b7355601cabc226cdc1da451791a4
SarikViper <dzamolovsardor5@gmail.com>
2025-03-11 19:48:52 +0500

    done validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,12 @@ import { Input } from "@/components/ui/input";
 import { useForm, SubmitHandler } from "react-hook-form"
 import { useNavigate } from "react-router-dom";
 
+
+const regex = {
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+  };
+
 type Inputs = {
     email: string
     password: string
@@ -15,22 +21,27 @@ const LoginPage = () => {
         handleSubmit,
         formState: { errors },
       } = useForm<Inputs>()
-      const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+      const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log("Form Data:", data);
+        navigate("/home");
+    };
 
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-25 w-full h-screen">
         <img src="/logo.png" alt="" />
-        <div className="flex flex-col max-w-[407] items-center justify-center gap-15">
+        <div className="flex flex-col max-w-[407px] items-center justify-center gap-15">
             <div className="max-w-[360px]">
           <p className="text-3xl font-bold">Sign in to your account</p>
             </div>
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-7">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-5">
             <label>
-              <Input {...register("email", {required:true})} className="w-[360px] h-[50px] text-center text-2xl" type="email" placeholder="Email" />
+              <Input {...register("email", {required:true, pattern:regex.email})} className="w-[360px] h-[50px] text-center text-2xl" type="email" placeholder="Email" />
+              {errors.email && <span className="text-red-500">Invalid Email</span>}
             </label>
             <label> 
-              <Input {...register("password", {required:true})} className="w-[360px] h-[50px] text-center text-2xl" type="password" placeholder="Password" />
+              <Input {...register("password", {required:true, pattern:regex.password})} className="w-[360px] h-[50px] text-center text-2xl" type="password" placeholder="Password" />
+              {errors.password && <span className="text-red-500">Invalid Password</span>}
             </label>
             <div className="flex items-center w-[350px] justify-between">
               <label className="flex gap-2">
@@ -41,7 +52,7 @@ const LoginPage = () => {
                 <a>Forgot password?</a>
               </label>
             </div>
-            <Button onClick={() => navigate('/home')} type="submit" className="w-[360px] h-[50px] bg-blue-600 text-white hover:bg-blue-900 cursor-pointer ">Sign In</Button>
+            <Button type="submit" className="w-[360px] h-[50px] bg-blue-600 text-white hover:bg-blue-900 cursor-pointer ">Sign In</Button>
           </form>
         </div>
       </div>
